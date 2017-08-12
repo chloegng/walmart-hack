7818e78155d8ff84d4f56bb26bc741cc0a712eb5
Gears23 <30958022+Gears23@users.noreply.github.com>
2017-08-12 14:12:40 -0700

    ...

--- Hackathon.js
+++ Hackathon.js
@@ -1,1 +1,5 @@
-alert ("test");
+alert ("JS is working!");
+
+var cards = document.querySelectorAll(".square");
+var itemName = document.querySelectorAll(".itemName");
+var salePrice = document.querySelector(".salePrice");
