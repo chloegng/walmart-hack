4dc953f7d24dd9260db6f263289da53e3a6389d1
Israel Torres <itorres1490@gmail.com>
2017-08-12 13:42:34 -0700

    Removing hackathon.js

--- hackathon.js
+++
@@ -1,5 +0,0 @@
-alert ("JS is working!");
-
-var cards = document.querySelectorAll(".square");
-var itemName = document.querySelectorAll(".itemName");
-var salePrice = document.querySelector(".salePrice");
