868898827f1d4a6dde1ece3eded73a770b3d652e
Nick Oliver <thenickoliver@gmail.com>
2017-08-12 15:35:28 -0700

    created callback function for getItems

--- app.js
+++ app.js
@@ -9,99 +9,120 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index');
+    var options = {
+        "params": {
+            "categoryId": 3944,
+            "sort": "price",
+            "query": "iPod"
+        }
+    }
+
+    getItems(options, function(jsonRes) {
+        res.render('index', {
+            items: jsonRes.items
+        });
+    });
 });
 
 // Get individual item by itemKey
-app.get('/getItem/:itemKey', function (req, res) {
-  var options = {
-    host: walmartAPI,
-    path: encodeURI("/v1/items/"+req.params.itemKey+"?apiKey="+apiKey+"&format=json")
-  }
+app.get('/getItem/:itemKey', function(req, res) {
+    var options = {
+        host: walmartAPI,
+        path: encodeURI("/v1/items/" + req.params.itemKey + "?apiKey=" + apiKey + "&format=json")
+    }
 
-  var request = http.get(options, function(response) {
-    var bodyChunks =[];
-    response.on('data', function(chunk){
-      bodyChunks.push(chunk);
-    }).on('end', function() {
-      var body = Buffer.concat(bodyChunks);
+    var request = http.get(options, function(response) {
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
 
-      var jsonRes = JSON.parse(body);
-      var item = {};
+            var jsonRes = JSON.parse(body);
+            var item = {};
 
-      item.itemId = jsonRes.itemId;
-      item.name = jsonRes.name;
-      item.salePrice = jsonRes.salePrice;
-      item.msrp = jsonRes.msrp;
-      item.shortDescription = jsonRes.shortDescription;
-      item.brandName = jsonRes.brandName;
-      item.thumbnailImage = jsonRes.thumbnailImage;
-      item.mediumImage = jsonRes.mediumImage;
-      item.largeImage = jsonRes.largeImage;
-      item.freeShipToStore = jsonRes.freeShipToStore;
-      item.numReviews = jsonRes.numReviews;
-      item.customerRating = jsonRes.customerRating;
-      item.customerRatingImage = jsonRes.customerRatingImage;
-      item.freeShippingOver50Dollars = jsonRes.freeShippingOver50Dollars;
-      item.imageEntities = jsonRes.imageEntities;
+            item.itemId = jsonRes.itemId;
+            item.name = jsonRes.name;
+            item.salePrice = jsonRes.salePrice;
+            item.msrp = jsonRes.msrp;
+            item.shortDescription = jsonRes.shortDescription;
+            item.brandName = jsonRes.brandName;
+            item.thumbnailImage = jsonRes.thumbnailImage;
+            item.mediumImage = jsonRes.mediumImage;
+            item.largeImage = jsonRes.largeImage;
+            item.freeShipToStore = jsonRes.freeShipToStore;
+            item.numReviews = jsonRes.numReviews;
+            item.customerRating = jsonRes.customerRating;
+            item.customerRatingImage = jsonRes.customerRatingImage;
+            item.freeShippingOver50Dollars = jsonRes.freeShippingOver50Dollars;
+            item.imageEntities = jsonRes.imageEntities;
 
-      res.type('application/json');
-      res.send(item);
+            res.type('application/json');
+            res.send(item);
+        })
+
     })
-
-  })
 })
 
 // Get array of items
-app.get('/getItems/:categoryId/:sort/:query', function (req, res) {
-  var categoryId = req.params.categoryId;
-  var sort = req.params.sort;
-  var query = req.params.query;
-  var options = {
-    host: walmartAPI,
-    path: encodeURI("/v1/search?apiKey="+apiKey+"&categoryId="+categoryId+"&sort="+sort+"&query="+query+"&facet=on&facet.range=price:[100 TO 500]")
-  }
-
-// Grabbing the shortened description from walmartAPI
-  var request = http.get(options, function(response) {
-    var bodyChunks =[];
-    response.on('data', function(chunk){
-      bodyChunks.push(chunk);
-    }).on('end', function() {
-      var body = Buffer.concat(bodyChunks);
-
-      var jsonRes = JSON.parse(body);
-      var items = [];
-      jsonRes.items.forEach(function(item){
-        var temp = {};
-
-        temp.itemId = item.itemId;
-        temp.name = item.name;
-        temp.salePrice = item.salePrice;
-        temp.msrp = item.msrp;
-        temp.shortDescription = item.shortDescription;
-        temp.brandName = item.brandName;
-        temp.thumbnailImage = item.thumbnailImage;
-        temp.mediumImage = item.mediumImage;
-        temp.largeImage = item.largeImage;
-        temp.freeShipToStore = item.freeShipToStore;
-        temp.numReviews = item.numReviews;
-        temp.customerRating = item.customerRating;
-        temp.customerRatingImage = item.customerRatingImage;
-        temp.freeShippingOver50Dollars = item.freeShippingOver50Dollars;
-        temp.imageEntities = item.imageEntities;
-
-        items.push(temp);
-      });
-        jsonRes.items = items;
-
-      res.type('application/json');
-      res.send(jsonRes);
-    })
-
-  })
+app.get('/getItems/:categoryId/:sort/:query', function(req, res) {
+    getItems(req, res, function(jsonRes) {
+        res.type('application/json');
+        res.send(jsonRes);
+    });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+// getItems callback function
+function getItems(req, callback) {
+    var categoryId = req.params.categoryId;
+    var sort = req.params.sort;
+    var query = req.params.query;
+
+    var options = {
+      host: walmartAPI,
+      path: encodeURI("/v1/search?apiKey=" + apiKey + "&categoryId=" + categoryId + "&sort=" + sort + "&query=" + query + "&facet=on&facet.range=price:[100 TO 500]")
+    }
+
+    // Grabbing the shortened description from walmartAPI
+    var request = http.get(options, function(response) {
+      var bodyChunks = [];
+      response.on('data', function(chunk) {
+          bodyChunks.push(chunk);
+      }).on('end', function() {
+          var body = Buffer.concat(bodyChunks);
+
+          var jsonRes = JSON.parse(body);
+          var items = [];
+
+          jsonRes.items.forEach(function(item) {
+              var temp = {};
+              temp.itemId = item.itemId;
+              temp.name = item.name;
+              temp.salePrice = item.salePrice;
+              temp.msrp = item.msrp;
+              temp.shortDescription = item.shortDescription;
+              temp.brandName = item.brandName;
+              temp.thumbnailImage = item.thumbnailImage;
+              temp.mediumImage = item.mediumImage;
+              temp.largeImage = item.largeImage;
+              temp.freeShipToStore = item.freeShipToStore;
+              temp.numReviews = item.numReviews;
+              temp.customerRating = item.customerRating;
+              temp.customerRatingImage = item.customerRatingImage;
+              temp.freeShippingOver50Dollars = item.freeShippingOver50Dollars;
+              temp.imageEntities = item.imageEntities;
+
+              items.push(temp);
+          });
+          jsonRes.items = items;
+
+          return callback(jsonRes);
+      })
+    })
+
+    return null;
+}
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
 })
